55bc268e1cd6bc60cd4489212f6d3fafb9c5b908
Caroline-Patrick <carolinespatrick@gmail.com>
2023-02-20 20:14:57 -0600

    added routes for cars and attached links

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import cars from '../cars.json'
+import cars from '../cars.json';
+import {useParams} from "react-router-dom";
 // Import {useParams} from "react-router-dom" here //
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    const {id} = useParams();
+    // params are what id is passed in - we can name the id object whatever we want in the router file
+  
+    const car = cars.find(car=> car.id === +id);
+    //+ sign in front of id -> forces string into number - could parse
+
+    console.log(car)
     return (
-        <h1>A specific car</h1>
+       <li>
+        <h2>{car.Name}</h2>
+        <p>{car.Miles_per_Gallon}</p>
+    </li> 
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import {BrowserRouter} from 'react-router-dom';
+
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>);
+    //wrap in browserrouter to give react router dom access to full DOM
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
